63bbdc4f290b8d84399a3d1c8aa9d08ea3462aea
Joseph D Curtis <joecurt85@gmail.com>
2016-04-17 09:46:22 -0400

    update collection.js
    Update collection.js and try to add null and undefined filters, but it's nested inside so I am trying to figure out how to loop and pluck those, possibly lodash, or should I map it on each collection?

--- collection.js
+++ collection.js
@@ -11,21 +11,21 @@ _.mixin(require("lodash-deep"));
 
 
 /* Dealing with a 2 dimensional array; ie
-*
-*
-* Learn a little bit about Immutable data and for manipulation.
-* https://facebook.github.io/immutable-js/
-*
-*
-* * https://www.cs.cmu.edu/~mrmiller/15-110/Handouts/arrays2D.pdf
-*{
-*   [ riData: [
-*   data
-*     ]
-*   ]
-* }
-* super helpful because the data was a multi dimensional array.  http://stackoverflow.com/questions/10865025/merge-flatten-a-multidimensional-array-in-javascript
-* */
+ *
+ *
+ * Learn a little bit about Immutable data and for manipulation.
+ * https://facebook.github.io/immutable-js/
+ *
+ *
+ * * https://www.cs.cmu.edu/~mrmiller/15-110/Handouts/arrays2D.pdf
+ *{
+ *   [ riData: [
+ *   data
+ *     ]
+ *   ]
+ * }
+ * super helpful because the data was a multi dimensional array.  http://stackoverflow.com/questions/10865025/merge-flatten-a-multidimensional-array-in-javascript
+ * */
 
 //Break this out into seperate files
 
@@ -50,26 +50,48 @@ const storeObj = store;
 //not immutable yet.
 let immutableArray = [];
 
+
+function removeUnwantedChar(arrayFilter) {
+
+}
+
 //Currently the only thing that was ACTUALLY needed was a simple forEach loop.
 
-function mapMyObj (array, pushArr) {
+function getLMapRequired (array, pushArr) {
   return array.forEach((element, index, value) => {
+    return array.filter(function (element) {
+
+    });
     pushArr.push(element[26]);
   });
 }
 
-mapMyObj(store.riData, immutableArray);
+function getLMapLatLng (immuteArray, pushArr) {
+  return immuteArrayArray.forEach((element, index, value) => {
+  })
+}
 
-console.log(immutableArray);
+getLMapRequired(store.riData, immutableArray);
+
+var removeZeros = function (arr) {
+  return arr.filter(function(element){
+    if(element!==0){
+      return element;
+    }
+  });
+}
+
+console.log(removeZeros(immutableArray));
+
 
 /* From the deepMap library https://www.npmjs.com/package/lodash-deep
-deepMap
+ deepMap
  */
 
 function deepMap(obj) {
   return _.deepMapValues(obj, function (value, path) {
     return value;
-    });
+  });
 }
 
 function turnIntoArray(obj) {
@@ -115,7 +137,7 @@ function mergeImmutableDeep (coll) {
 
 /* My settings would be.  const filePathRIJSON = '/Users/jcurtis/Desktop/newJson.json';
  * refactor this
-*/
+ */
 
 const filePathRIJSON = '/Users/jcurtis/Desktop/newJson.json';
 
